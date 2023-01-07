24a126f4b9a9cd3b408611cb1066abbf38143a93
KodchakonKonfai02 <Kodchakonkonfai0@gmail.com>
2023-01-07 09:54:28 +0700

    new many file

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+    $servername = "localhost";
+    /*$username = "it65309010001";
+    $password = "it12345";
+    $dbname = "it65309010001";*/
+
+    $username = "root";
+    $password = "12345678";
+    $dbname = "wed2111";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    mysqli_set_charset($conn,"utf8");
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+?>
